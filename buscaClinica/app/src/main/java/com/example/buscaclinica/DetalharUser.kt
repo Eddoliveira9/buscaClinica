fdc6792621ce3c58a7ff172505bd3c48f88c4bc1
alancarvalhobrito <alan_carvalhobrito@hotmail.com>
2019-05-13 21:07:53 -0300

    Implementação da tela de detalhamento do usuário

---
+++ buscaClinica/app/src/main/java/com/example/buscaclinica/DetalharUser.kt
@@ -0,0 +1,12 @@
+package br.com.iesb.projetoteste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalharUser : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhar_user)
+    }
+}
