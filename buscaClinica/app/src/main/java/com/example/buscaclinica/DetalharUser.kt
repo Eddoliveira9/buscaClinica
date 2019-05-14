5cfc1c024ed1ae4333e7733b85b42fcc074e84a4
alancarvalhobrito <alan_carvalhobrito@hotmail.com>
2019-05-13 21:10:33 -0300

    Setando valores fixos no formulário

--- buscaClinica/app/src/main/java/com/example/buscaclinica/DetalharUser.kt
+++ buscaClinica/app/src/main/java/com/example/buscaclinica/DetalharUser.kt
@@ -8,5 +8,11 @@ class DetalharUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhar_user)
+
+        editTextNomeCompleto.text = Editable.Factory.getInstance().newEditable("Jão da Silva")
+        editTextEmail.text = Editable.Factory.getInstance().newEditable("jao@silva.com")
+        editTextIdade.text = Editable.Factory.getInstance().newEditable("20")
+        editTextSexo.text = Editable.Factory.getInstance().newEditable("Masculino")
+        editTextSenha.text = Editable.Factory.getInstance().newEditable("senhaboa")
     }
 }
