75ba51fc70a2a158a037fa235e81d4f5cd0ecacf
diego <diegoleitecarvalho@hotmail.com>
2019-06-01 09:38:41 -0300

    alterar projeto para a versao 2, com a nova arquitetura

---
+++ buscaClinica2/app/src/main/java/com/projeto/buscaclinica/activity/splashActivity.kt
@@ -0,0 +1,14 @@
+package com.projeto.buscaclinica.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projeto.buscaclinica.R
+
+
+class splashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
