4e06b9d5029639b3db926ae084f07136ae27974d
Eddoliveira9 <eddoliveira9@gmail.com>
2019-06-04 20:30:33 -0300

    Adicionando o google.services a splash 2s e corrigindo o android manifest

--- buscaClinica/app/src/main/java/com/projeto/buscaclinica/activity/splashActivity.kt
+++ buscaClinica/app/src/main/java/com/projeto/buscaclinica/activity/splashActivity.kt
@@ -1,8 +1,11 @@
 package com.projeto.buscaclinica.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.projeto.buscaclinica.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class splashActivity : AppCompatActivity() {
@@ -10,5 +13,21 @@ class splashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        buttonPrevious.visibility = View.GONE
+
+        buttonNext.setOnClickListener {
+            canvasOnboarding.next { finish ->
+                if (finish) {
+                    val login = Intent(this@splashActivity, LoginActivity::class.java)
+                    startActivity(login)
+                    finish()
+                }
+            }
+        }
+
+        buttonPrevious.setOnClickListener {
+            canvasOnboarding.previous()
+        }
     }
 }
